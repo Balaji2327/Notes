d9461436e5cf1349b2c3e1de1d944f866e7b27a7
Balaji R <155892613+Balaji2327@users.noreply.github.com>
2025-10-30 22:22:38 +0530

    Update AppBar background color to enhance UI aesthetics

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         scaffoldBackgroundColor: Colors.white,
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
+          backgroundColor: Color.fromARGB(255, 227, 111, 111),
           foregroundColor: Colors.black,
           elevation: 0,
           centerTitle: true,
